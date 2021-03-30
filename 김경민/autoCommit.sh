6c14aae2b278f136186461af92774a1a3206a98e
Kyoungmin Kim <kkm1447@naver.com>
2021-03-30 10:43:31 +0900

    [21.3.30]autoCommit.sh

--- 김경민/autoCommit.sh
+++ 김경민/autoCommit.sh
@@ -1,7 +1,6 @@
-git pull && git add . && gitStatus=`git status` && fileName=`java commitMessage $gitStatus` &&
-echo "1"
-echo $gitStatus
-echo "2"
-echo $fileName
-echo "3"
-git commit -m "$fileName" && git push
+git pull &&
+git add . &&
+gitStatus=`git status` &&
+fileName=`java commitMessage $gitStatus` &&
+git commit -m "$fileName" &&
+git push
--- 김경민/commitMessage.java
+++ 김경민/commitMessage.java
@@ -2,18 +2,10 @@ import java.time.LocalDate;
 public class commitMessage {
   public static void main(String args[]) {
     LocalDate currentDate = LocalDate.now();
-    String split[] = args[0].split("\n");
-    for (String line : split) {
-      System.out.println(line + "1");
-      if (line.contains("new file:") || line.contains("modified:")) {
-        String lines[] = line.split(" ");
-        String fileName = "";
-        for (String temp : lines) {
-          if (temp.length() > 0 && temp.charAt(0) != '\t' && !temp.equals("file:")) {
-            fileName = temp;
-            break;
-          }
-        }
+    for (int i = 0; i < args.length; i += 1) {
+      String line = args[i];
+      if (line.equals("file:") || line.equals("modified:")) {
+        String fileName = args[i + 1];
         System.out.println("[" + currentDate.getYear() % 100 + "." + currentDate.getMonthValue() + "." + currentDate.getDayOfMonth() + "]" + fileName);
         break;
       }
