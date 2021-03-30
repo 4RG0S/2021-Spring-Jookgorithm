cb7abd0497d51955736246b53fe5a51c1999f592
Kyoungmin Kim <kkm1447@naver.com>
2021-03-30 10:35:54 +0900

    On1

--- 김경민/autoCommit.sh
+++ 김경민/autoCommit.sh
@@ -1,5 +1,7 @@
-git add .
-gitStatus=`git status`
-fileName=`java commitMessage $gitStatus`
-git commit -m "$fileName"
-git push
+git pull && git add . && gitStatus=`git status` && fileName=`java commitMessage $gitStatus` &&
+echo "1"
+echo $gitStatus
+echo "2"
+echo $fileName
+echo "3"
+git commit -m "$fileName" && git push
--- 김경민/commitMessage.java
+++ 김경민/commitMessage.java
@@ -4,11 +4,12 @@ public class commitMessage {
     LocalDate currentDate = LocalDate.now();
     String split[] = args[0].split("\n");
     for (String line : split) {
+      System.out.println(line + "1");
       if (line.contains("new file:") || line.contains("modified:")) {
         String lines[] = line.split(" ");
         String fileName = "";
         for (String temp : lines) {
-          if (temp.length() > 0 && temp.charAt(0) != '\t') {
+          if (temp.length() > 0 && temp.charAt(0) != '\t' && !temp.equals("file:")) {
             fileName = temp;
             break;
           }
