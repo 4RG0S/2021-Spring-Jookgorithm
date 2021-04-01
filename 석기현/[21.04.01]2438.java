3af6df703243e1c1b4d1f0efadfe257b07438405
석기현 <sgh45683856@gmail,com>
2021-04-01 19:24:34 +0900

    21.04.01

---
+++ 석기현/[21.04.01]2438.java
@@ -0,0 +1,24 @@
+package ClubHomework;
+
+import java.util.Scanner;
+
+public class GiHyeon2438 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a, i, j;
+		a = sc.nextInt();
+		
+		for(i = 0; i < a; i++) {
+			for(j=0;j<i+1;j++) {
+			System.out.print("*");
+			}
+			System.out.print("\n");
+	}
+		
+	}
+
+}
